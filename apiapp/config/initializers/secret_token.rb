b6c4b0e9793f6e75a569e7111023d537cf86f892
RailsFactory <senthil@railsfactory.org>
2011-01-19 16:11:51 +0530

    added empty client and server apps

---
+++ apiapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apiapp::Application.config.secret_token = 'd4c79aa683aef0503cdb627067515961b42ed461cc39931446aebbae730e5383902d7e8dd739ba1446b7711032933011483cb400b353dfb040a72da0faaac984'
---
+++ clientapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clientapp::Application.config.secret_token = '8bd8760036b5f49540e99202fbb4a5ac254e2b89dae3d0aa5584516e5602ac2191d4f33a92eab11f3a3bbe9baab363771b54f769c040b024aefcb8ae792ed9ec'
